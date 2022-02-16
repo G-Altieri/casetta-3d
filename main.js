6c0c6e772f37237d62e2a1577bbf2f45cd83aa65
Giovanni Altieri <giovanni.alt1@gmail.com>
2022-02-16 01:46:07 +0100

    Test Fake Casetta

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 import './style.css';
-import lax from 'lax.js'
+//import lax from 'lax.js'
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls';
 //import { GLTFLoader } from 'three/examples/jsm/loader/GLTFLoader.js';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import {
+    OBJLoader
+} from 'three/examples/jsm/loaders/OBJLoader.js';
+import {
+    MTLLoader
+} from 'three/examples/jsm/loaders/MTLLoader.js';
 // Setup
 const scene = new THREE.Scene();
 
@@ -20,11 +26,12 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
+camera.position.setZ(100);
+camera.position.setX(0);
+camera.position.setY(15);
 
 //Render Resize the windows 
-window.addEventListener('resize', function () {
+window.addEventListener('resize', function() {
     var width = window.innerWidth;
     var height = window.innerHeight;
     renderer.setSize(width, height);
@@ -34,50 +41,121 @@ window.addEventListener('resize', function () {
 })
 renderer.render(scene, camera);
 
-//Geometry
-const geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
-//const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-//const material = new THREE.MeshStandardMaterial({ color: 0xffff0, wireframe:false});
-const material = new THREE.MeshNormalMaterial({ color: 0xffff0, wireframe: false });
-const torus = new THREE.Mesh(geometry, material);
-
-//scene.add(torus);
 
 // Lights
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(20, 30, 20);
+pointLight.position.set(20, 30, 0);
 const lightHelper2 = new THREE.PointLightHelper(pointLight);
 
 const pointLight2 = new THREE.PointLight(0xffffff);
-pointLight2.position.set(0, 0, -20);
+pointLight2.position.set(0, 50, -20);
 const lightHelper3 = new THREE.PointLightHelper(pointLight2);
 
 const ambientLight = new THREE.AmbientLight(0x404040);
-ambientLight.intensity = 0.8;
+ambientLight.intensity = 4;
 const lightHelper = new THREE.PointLightHelper(ambientLight);
 
-scene.add(pointLight,pointLight2, ambientLight);
+scene.add(pointLight, pointLight2, ambientLight);
 
 const gridHelper = new THREE.GridHelper(200, 50);
-//scene.add(lightHelper, lightHelper2,lightHelper3);
+scene.add(lightHelper, lightHelper2, lightHelper3, gridHelper);
 
 //Controls
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableZoom = false;
+controls.enableZoom = true;
 
 //bg
 const bg = new THREE.TextureLoader().load('/assets/img/bg.jpg');
-//scene.background = bg;
+scene.background = bg;
 
 
+//Loading Model
+const objLoader = new OBJLoader()
+const objLoader2 = new OBJLoader()
+const mtlLoader = new MTLLoader()
+const mtlLoader2 = new MTLLoader()
+
+
+//Casetta
+mtlLoader.load(
+    '/assets/model/fake-casetta-obg/fakecasetta.mtl',
+    (materials) => {
+        materials.preload()
+        objLoader.setMaterials(materials)
+        objLoader.load(
+            '/assets/model/fake-casetta-obg/fakecasetta.obj',
+            (object) => {
+                casetta(object)
+                scene.add(object)
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded obj Casetta')
+            },
+            (error) => {
+                console.log('An error happened')
+            }
+        )
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded mtl Casetta')
+    },
+    (error) => {
+        console.log('An error happened2')
+    }
+)
+
+//Grass
+mtlLoader2.load(
+    '/assets/model/grass/grass.mtl',
+    (materials) => {
+        materials.preload()
+        objLoader2.setMaterials(materials)
+        objLoader2.load(
+            '/assets/model/grass/grass.obj',
+            (object) => {
+                grass(object)
+                scene.add(object);
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded obj Grass')
+            },
+            (error) => {
+                console.log('An error happened')
+            }
+        )
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded mtl Grass')
+    },
+    (error) => {
+        console.log('An error happened2')
+    }
+)
+
+
+function casetta(object) {
+    object.position.y = 8;
+}
+
+function grass(object) {
+    object.rotation.x = 300;
+}
+
+
+var angle = 0;
+var radius = 100;
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate);
 
     //Movement
-    torus.rotation.x += 0.005;
-    //  torus.rotation.y += 0.005;
-    torus.rotation.z += 0.005;
+    //camera.position.setX(x += 0.1 * Math.sin(1));
+
+    camera.position.x = radius * Math.cos(angle);
+    camera.position.z = radius * Math.sin(angle);
+    angle += 0.003;
+
+
 
     controls.update();
 
@@ -87,112 +165,68 @@ function animate() {
 animate();
 
 
-//GLTFLoader
-// instantiate a loader Objects
-const loader = new OBJLoader();
-var Knife2;
-// load a resource
-loader.load(
-    // resource URL
-    '/assets/model/Knife_obj/Knife_obj.obj',
-    // called when resource is loaded
-    function (object) {
-        scene.add(object);
-        Knife2=object;
-        Knife(object);
-        console.log("Caricato");
-    },
-    // called when loading is in progresses
-    function (xhr) {
-        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    // called when loading has errors
-    function (error) {
-        console.log('An error happened');
-    }
-);
 
-
-
-function Knife(object){
-    object.rotation.x =-24;
-}
-
-
-document.getElementById("myBtn").addEventListener("click", function() {
-    Knife2.rotation.z =-300;
-    setTimeout(()=>{
-        Knife2.rotation.y =-100;
-
-    }, 1000);
-});
-
-window.onload = function () {
-    lax.init()
-
-    // Add a driver that we use to control our animations
-    lax.addDriver('scrollY', function () {
-      return window.scrollY
-    })
-
-    // Add animation bindings to elements
-    lax.addElements('.button5', {
-      scrollY: {
-        translateX: [
-          ["elInY", "elCenterY", "elOutY"],
-          [0, 'screenWidth/2', 'screenWidth'],
-        ]
-      }
-    })
-  }
 /*
-const mtlLoader = new MTLLoader()
-mtlLoader.load(
-    '/assets/model/Knife_obj/Knife_obj.mtl',
-    (materials) => {
-        materials.preload()
-
-        const objLoader = new OBJLoader()
-        objLoader.setMaterials(materials)
-        objLoader.load(
-            '/assets/model/Knife_obj/Knife_obj.obj',
-            (object) => {
-                scene.add(object)
-            },
-            (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-            },
-            (error) => {
-                console.log('An error happened')
-            }
-        )
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log('An error happened')
-    }
-)*/
-
-
-
-  /**
- * Animate
- */
 const clock = new THREE.Clock()
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime()
 
-  //mesh.rotation.x += 0.01 * Math.sin(1)
-  //mesh.rotation.y += 0.01 * Math.sin(1)
-  //mesh.rotation.z += 0.01 * Math.sin(1)
+    casetta2.rotation.x += 0.01 * Math.sin(1)
+    casetta2.rotation.y += 0.01 * Math.sin(1)
+    casetta2.rotation.z += 0.01 * Math.sin(1)
 
-  // Update controls
-  controls.update()
-  // Render
-  renderer.render(scene, camera)
+    // Update controls
+    controls.update()
+        // Render
+    renderer.render(scene, camera)
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick)
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
+*/
+
+
+/* 
+//BUTTON
+  document.getElementById("myBtn").addEventListener("click", function() {
+      Knife2.rotation.z = -300;
+      setTimeout(() => {
+          Knife2.rotation.y = -100;
+  
+      }, 1000);
+  });
+  */
+/* //SCROOL LIBRARY
+window.onload = function() {
+        lax.init()
+
+        // Add a driver that we use to control our animations
+        lax.addDriver('scrollY', function() {
+            return window.scrollY
+        })
+
+        // Add animation bindings to elements
+        lax.addElements('.button5', {
+            scrollY: {
+                translateX: [
+                    ["elInY", "elCenterY", "elOutY"],
+                    [0, 'screenWidth/2', 'screenWidth'],
+                ]
+            }
+        })
+    }*/
+
+
+/*
+//Geometry
+const geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
+//const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+//const material = new THREE.MeshStandardMaterial({ color: 0xffff0, wireframe:false});
+const material = new THREE.MeshNormalMaterial({
+    color: 0xffff0,
+    wireframe: false
+});
+const torus = new THREE.Mesh(geometry, material);
+
+//scene.add(torus);
+*/
